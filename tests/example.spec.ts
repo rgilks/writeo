8a8cd8b05104ae88b27faac7d3970e5e3a05d859
rgilks <robert.gilks@cambridge.org>
2025-06-11 09:54:30 +0100

    initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "@playwright/test";
+
+test('homepage has "Welcome to Writeo"', async ({ page }) => {
+  await page.goto("/");
+  await expect(page.locator("main")).toContainText("Welcome to Writeo");
+});
